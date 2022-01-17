5d6770688b3ef9278dc4b36795e93ff1370036ce
Ivan Jure Parać <72452622+iparac@users.noreply.github.com>
2022-01-17 03:18:10 +0000

    Program Engineering - Project

---
+++ Software/KindergartenJoy/Forme/FormGlavna.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace KindergartenJoy.Forme
+{
+    partial class FormGlavna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGlavna));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPrijava = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStatistika = new System.Windows.Forms.Button();
+            this.btnUpravljanjeUlogama = new System.Windows.Forms.Button();
+            this.btnBilješke = new System.Windows.Forms.Button();
+            this.btnEvidencija = new System.Windows.Forms.Button();
+            this.btnAktivnost = new System.Windows.Forms.Button();
+            this.btnObavijesti = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVrijeme = new System.Windows.Forms.Label();
+            this.lblIme = new System.Windows.Forms.Label();
+            this.pbProfil = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.btnNaslovnica = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(598, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 311);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(345, 190);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(207, 311);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dobrodošli!";
+            // 
+            // btnPrijava
+            // 
+            this.btnPrijava.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnPrijava.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPrijava.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrijava.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPrijava.Location = new System.Drawing.Point(5, 50);
+            this.btnPrijava.Name = "btnPrijava";
+            this.btnPrijava.Size = new System.Drawing.Size(222, 42);
+            this.btnPrijava.TabIndex = 11;
+            this.btnPrijava.Text = "UPIS DJECE";
+            this.btnPrijava.UseVisualStyleBackColor = false;
+            this.btnPrijava.Click += new System.EventHandler(this.btnPrijava_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.btnStatistika);
+            this.panel1.Controls.Add(this.btnUpravljanjeUlogama);
+            this.panel1.Controls.Add(this.btnBilješke);
+            this.panel1.Controls.Add(this.btnEvidencija);
+            this.panel1.Controls.Add(this.btnAktivnost);
+            this.panel1.Controls.Add(this.btnObavijesti);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnPrijava);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(228, 453);
+            this.panel1.TabIndex = 10;
+            // 
+            // btnStatistika
+            // 
+            this.btnStatistika.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnStatistika.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStatistika.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatistika.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStatistika.Location = new System.Drawing.Point(5, 380);
+            this.btnStatistika.Name = "btnStatistika";
+            this.btnStatistika.Size = new System.Drawing.Size(222, 42);
+            this.btnStatistika.TabIndex = 16;
+            this.btnStatistika.Text = "STATISTIKA";
+            this.btnStatistika.UseVisualStyleBackColor = false;
+            this.btnStatistika.Click += new System.EventHandler(this.btnStatistika_Click_1);
+            // 
+            // btnUpravljanjeUlogama
+            // 
+            this.btnUpravljanjeUlogama.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnUpravljanjeUlogama.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUpravljanjeUlogama.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpravljanjeUlogama.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnUpravljanjeUlogama.Location = new System.Drawing.Point(5, 325);
+            this.btnUpravljanjeUlogama.Name = "btnUpravljanjeUlogama";
+            this.btnUpravljanjeUlogama.Size = new System.Drawing.Size(224, 42);
+            this.btnUpravljanjeUlogama.TabIndex = 15;
+            this.btnUpravljanjeUlogama.Text = "UPRAVLJANJE ULOGAMA";
+            this.btnUpravljanjeUlogama.UseVisualStyleBackColor = false;
+            this.btnUpravljanjeUlogama.Click += new System.EventHandler(this.btnUpravljanjeUlogama_Click);
+            // 
+            // btnBilješke
+            // 
+            this.btnBilješke.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnBilješke.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBilješke.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBilješke.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBilješke.Location = new System.Drawing.Point(5, 270);
+            this.btnBilješke.Name = "btnBilješke";
+            this.btnBilješke.Size = new System.Drawing.Size(222, 42);
+            this.btnBilješke.TabIndex = 14;
+            this.btnBilješke.Text = "BILJEŠKE";
+            this.btnBilješke.UseVisualStyleBackColor = false;
+            this.btnBilješke.Click += new System.EventHandler(this.btnBilješke_Click);
+            // 
+            // btnEvidencija
+            // 
+            this.btnEvidencija.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnEvidencija.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEvidencija.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEvidencija.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEvidencija.Location = new System.Drawing.Point(5, 215);
+            this.btnEvidencija.Name = "btnEvidencija";
+            this.btnEvidencija.Size = new System.Drawing.Size(222, 42);
+            this.btnEvidencija.TabIndex = 13;
+            this.btnEvidencija.Text = "EVIDENCIJA";
+            this.btnEvidencija.UseVisualStyleBackColor = false;
+            this.btnEvidencija.Click += new System.EventHandler(this.btnEvidencija_Click);
+            // 
+            // btnAktivnost
+            // 
+            this.btnAktivnost.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnAktivnost.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAktivnost.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAktivnost.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAktivnost.Location = new System.Drawing.Point(5, 160);
+            this.btnAktivnost.Name = "btnAktivnost";
+            this.btnAktivnost.Size = new System.Drawing.Size(222, 42);
+            this.btnAktivnost.TabIndex = 12;
+            this.btnAktivnost.Text = "AKTIVNOSTI";
+            this.btnAktivnost.UseVisualStyleBackColor = false;
+            this.btnAktivnost.Click += new System.EventHandler(this.btnAktivnost_Click);
+            // 
+            // btnObavijesti
+            // 
+            this.btnObavijesti.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnObavijesti.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnObavijesti.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObavijesti.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnObavijesti.Location = new System.Drawing.Point(5, 105);
+            this.btnObavijesti.Name = "btnObavijesti";
+            this.btnObavijesti.Size = new System.Drawing.Size(222, 42);
+            this.btnObavijesti.TabIndex = 11;
+            this.btnObavijesti.Text = "OBAVIJESTI";
+            this.btnObavijesti.UseVisualStyleBackColor = false;
+            this.btnObavijesti.Click += new System.EventHandler(this.btnObavijesti_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(58, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Izbornik";
+            // 
+            // lblVrijeme
+            // 
+            this.lblVrijeme.AutoSize = true;
+            this.lblVrijeme.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVrijeme.Location = new System.Drawing.Point(771, 425);
+            this.lblVrijeme.Name = "lblVrijeme";
+            this.lblVrijeme.Size = new System.Drawing.Size(85, 16);
+            this.lblVrijeme.TabIndex = 11;
+            this.lblVrijeme.Text = "Vrijeme";
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblIme.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIme.Location = new System.Drawing.Point(318, 37);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(34, 13);
+            this.lblIme.TabIndex = 12;
+            this.lblIme.Text = "Ime";
+            this.lblIme.Click += new System.EventHandler(this.lblIme_Click);
+            // 
+            // pbProfil
+            // 
+            this.pbProfil.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pbProfil.Image = ((System.Drawing.Image)(resources.GetObject("pbProfil.Image")));
+            this.pbProfil.Location = new System.Drawing.Point(248, 18);
+            this.pbProfil.Name = "pbProfil";
+            this.pbProfil.Size = new System.Drawing.Size(60, 52);
+            this.pbProfil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbProfil.TabIndex = 13;
+            this.pbProfil.TabStop = false;
+            this.pbProfil.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(376, 120);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(116, 64);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(542, 83);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(116, 64);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // btnNaslovnica
+            // 
+            this.btnNaslovnica.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnNaslovnica.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNaslovnica.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNaslovnica.ForeColor = System.Drawing.Color.Black;
+            this.btnNaslovnica.Location = new System.Drawing.Point(518, 25);
+            this.btnNaslovnica.Name = "btnNaslovnica";
+            this.btnNaslovnica.Size = new System.Drawing.Size(135, 35);
+            this.btnNaslovnica.TabIndex = 27;
+            this.btnNaslovnica.Text = "Odjavi se";
+            this.btnNaslovnica.UseVisualStyleBackColor = false;
+            this.btnNaslovnica.Click += new System.EventHandler(this.btnNaslovnica_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel2.Controls.Add(this.btnNaslovnica);
+            this.panel2.Controls.Add(this.pbProfil);
+            this.panel2.Controls.Add(this.lblIme);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(221, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(680, 81);
+            this.panel2.TabIndex = 28;
+            // 
+            // FormGlavna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.lblVrijeme);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormGlavna";
+            this.Text = "FormGlavna";
+            this.Load += new System.EventHandler(this.FormGlavna_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPrijava;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStatistika;
+        private System.Windows.Forms.Button btnUpravljanjeUlogama;
+        private System.Windows.Forms.Button btnBilješke;
+        private System.Windows.Forms.Button btnEvidencija;
+        private System.Windows.Forms.Button btnAktivnost;
+        private System.Windows.Forms.Button btnObavijesti;
+        private System.Windows.Forms.Label lblVrijeme;
+        private System.Windows.Forms.Label lblIme;
+        private System.Windows.Forms.PictureBox pbProfil;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button btnNaslovnica;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ Software/KindergartenJoy/Klase/TipKorisnika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KindergartenJoy
+{
+    public partial class  tip_korisnik
+    {
+        public override string ToString()
+        {
+            return vrsta_korsnik;
+        }
+    }
+}
